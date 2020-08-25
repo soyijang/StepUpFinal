5ec7ed932a21d941844771b7e032eac8401d8824
misoKim <1004catherine@gmail.com>
2020-08-25 12:43:40 +0900

    merge

--- src/main/java/com/stepup/agile/userInfo/model/dao/MemberDaoImpl.java
+++ src/main/java/com/stepup/agile/userInfo/model/dao/MemberDaoImpl.java
@@ -1,5 +1,7 @@
 package com.stepup.agile.userInfo.model.dao;
 
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -8,24 +10,6 @@ import com.stepup.agile.userInfo.model.vo.Member;
 
 @Repository
 public class MemberDaoImpl implements MemberDao {
-
-	@Override
-	public String selectEncPassword(SqlSessionTemplate sqlSession, Member m) {
-
-		return sqlSession.selectOne("Member.selectPwd", m.getUserEmail());
-	}
-
-	@Override
-	public int insertMember(SqlSessionTemplate sqlSession, Member m) {
-
-		return sqlSession.insert("Member.insertMember", m);
-	}
-
-	@Override
-	public Member selectMember(SqlSessionTemplate sqlSession, Member m) {
-
-		return sqlSession.selectOne("Member.selectLoginUser", m);
-	}
 	
 	@Override
 	public Member loginCheck(SqlSessionTemplate sqlSession, Member m) throws LoginFailedException {
@@ -37,8 +21,33 @@ public class MemberDaoImpl implements MemberDao {
 		if(loginUser == null) {
 			throw new LoginFailedException("로그인 실패!");
 		}
-		
+		System.out.println("loginCheck : " + loginUser);
 		return loginUser;
+	}
+
+	@Override
+	public String selectEncPassword(SqlSessionTemplate sqlSession, Member m) {
+		System.out.println("member.selectPwd" + sqlSession.selectOne("Member.selectPwd", m.getUserEmail()));
+		return sqlSession.selectOne("Member.selectPwd", m.getUserEmail());
+	}
+
+	@Override
+	public int insertMember(SqlSessionTemplate sqlSession, Member m) {
+		
+		return sqlSession.insert("Member.insertMember", m);
+	}
+
+	@Override
+	public Member selectMember(SqlSessionTemplate sqlSession, Member m) {
+		System.out.println("member.selectLoginUSERT : " + sqlSession.selectOne("Member.selectLoginUser", m));
+		return sqlSession.selectOne("Member.selectLoginUser", m);
+	}
+	
+
+	@Override
+	public int insertJob(SqlSessionTemplate sqlSession, Map<String, Object> member) {
+
+		return sqlSession.update("Member.insertJob", member);
 	}
 
 }
