c7670f202bf433e5c6b3093f679589111ce11ade
다윤 <ekdbsekdbs@gmail.com>
2020-08-26 16:37:37 +0900

    문서 추가 : projectController 추가중
    projectController 추가중

--- src/main/java/com/stepup/agile/projectManage/controller/ProjectManageController.java
+++ src/main/java/com/stepup/agile/projectManage/controller/ProjectManageController.java
@@ -1,11 +1,16 @@
 package com.stepup.agile.projectManage.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.stepup.agile.projectManage.model.service.ProjectService;
+import com.stepup.agile.projectManage.model.vo.Project;
+import com.stepup.agile.userInfo.model.vo.Member;
 
 @SessionAttributes("loginUser")
 @Controller
@@ -13,8 +18,13 @@ public class ProjectManageController {
 	@Autowired
 	private ProjectService ps;
 	
-	public String selectProject(@("loginUser")Model model, ) {
+	public String selectProject(@ModelAttribute("loginUser")Member m, Model model) {
 		
-		return "";
+		List<Project> pjList = ps.selectProject(m);
+		
+		model.addAttribute("pjList", pjList);
+		
+		
+		return "projectManage/projectTimeLine/projectTimeLine";
 	}
 }
--- src/main/java/com/stepup/agile/projectManage/model/service/ProjectService.java
+++ src/main/java/com/stepup/agile/projectManage/model/service/ProjectService.java
@@ -1,6 +1,12 @@
 package com.stepup.agile.projectManage.model.service;
 
+import java.util.List;
+
+import com.stepup.agile.projectManage.model.vo.Project;
+import com.stepup.agile.userInfo.model.vo.Member;
+
 public interface ProjectService {
 
+	List<Project> selectProject(Member m);
 	
 }
