e88464874318a15b6ecbc86813dd4f9136e8999c
misoKim <1004catherine@gmail.com>
2020-08-31 11:24:00 +0900

    문서 수정 : project main 페이지
    프로젝트 정보 조회 기능

---
+++ src/main/java/com/stepup/agile/projectManage/model/dao/ProjectDaoImpl.java
@@ -0,0 +1,34 @@
+package com.stepup.agile.projectManage.model.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.stepup.agile.projectManage.model.vo.Project;
+import com.stepup.agile.userInfo.model.vo.Member;
+
+@Repository
+public class ProjectDaoImpl implements ProjectDao{
+
+	
+	//프로젝트 생성
+	@Override
+	public int insertProject(SqlSessionTemplate sqlSession, Project p) {
+		return sqlSession.insert("Project.insertProject", p);
+	}
+	
+	//프로젝트 메인페이지로 포워딩 (해당 멤버의 project list 조회 후 view 이동) 
+	@Override
+	public List<Project> selectProjectList(SqlSessionTemplate sqlSession, Member m) {
+		//System.out.println("ProjectDao m : " + m);
+		return sqlSession.selectList("Project.selectProjectList", m);
+	}
+
+	//진행률 계산식
+	@Override
+	public HashMap<String, Integer> selectProjectProceedingRate(SqlSessionTemplate sqlSession, int[] projectCodeArr) {
+		return (HashMap<String, Integer>) sqlSession.selectList("Project.selectProjectList", projectCodeArr);
+	}
+}
--- src/main/java/com/stepup/agile/projectManage/model/vo/Project.java
+++ src/main/java/com/stepup/agile/projectManage/model/vo/Project.java
@@ -1,5 +1,11 @@
 package com.stepup.agile.projectManage.model.vo;
 
+import com.stepup.agile.projectBacklog.model.vo.Sprint;
+import com.stepup.agile.projectBacklog.model.vo.SprintHistory;
+import com.stepup.agile.userInfo.model.vo.Member;
+import com.stepup.agile.userInfo.model.vo.Team;
+import com.stepup.agile.userInfo.model.vo.UserProjectList;
+import com.stepup.agile.userInfo.model.vo.UserTeamList;
 
 public class Project implements java.io.Serializable{
 	private int projectCode;
@@ -7,15 +13,36 @@ public class Project implements java.io.Serializable{
 	private String createDate;
 	private String createTime;
 	
+	private ProjectHistory projectHistory;
+	private UserProjectList userProjectList;
+	private UserTeamList userTeamList;
+	private Member member;
+	private Team team;
+
 	public Project() {}
 
-	public Project(int projectCode, String projectName, String createDate, String createTime) {
+
+
+
+
+	public Project(int projectCode, String projectName, String createDate, String createTime,
+			ProjectHistory projectHistory, UserProjectList userProjectList, UserTeamList userTeamList, Member member,
+			Team team) {
 		super();
 		this.projectCode = projectCode;
 		this.projectName = projectName;
 		this.createDate = createDate;
 		this.createTime = createTime;
+		this.projectHistory = projectHistory;
+		this.userProjectList = userProjectList;
+		this.userTeamList = userTeamList;
+		this.member = member;
+		this.team = team;
 	}
+
+
+
+
 
 	public int getProjectCode() {
 		return projectCode;
@@ -49,11 +76,51 @@ public class Project implements java.io.Serializable{
 		this.createTime = createTime;
 	}
 
+
+	public ProjectHistory getProjectHistory() {
+		return projectHistory;
+	}
+
+	public void setProjectHistory(ProjectHistory projectHistory) {
+		this.projectHistory = projectHistory;
+	}
+
+	public UserProjectList getUserProjectList() {
+		return userProjectList;
+	}
+
+	public void setUserProjectList(UserProjectList userProjectList) {
+		this.userProjectList = userProjectList;
+	}
+
+	public UserTeamList getUserTeamList() {
+		return userTeamList;
+	}
+
+	public void setUserTeamList(UserTeamList userTeamList) {
+		this.userTeamList = userTeamList;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
 	@Override
 	public String toString() {
-		return "ProjectList [projectCode=" + projectCode + ", projectName=" + projectName + ", createDate=" + createDate
-				+ ", createTime=" + createTime + "]";
+		return "Project [projectCode=" + projectCode + ", projectName=" + projectName + ", createDate=" + createDate
+				+ ", createTime=" + createTime + ", projectHistory=" + projectHistory + ", userProjectList="
+				+ userProjectList + ", userTeamList=" + userTeamList + ", member=" + member + ", team=" + team + "]";
 	}
-	
-	
 }
