a547038be5a2719f6fbd09d53cbc42ee71b1a3da
misoKim <1004catherine@gmail.com>
2020-09-07 10:38:55 +0900

    문서 수정 : taskBoard 관련 조회 기능

--- src/main/java/com/stepup/agile/projectTask/model/service/TaskServiceImpl.java
+++ src/main/java/com/stepup/agile/projectTask/model/service/TaskServiceImpl.java
@@ -85,4 +85,12 @@ public class TaskServiceImpl implements TaskService {
 		return td.searchBug(sqlSession, map);
 	}
 
+	
+	//miso Kim's task ------------------------------------------------------------------------------------
+	//테스크 리스트 조회 후 보드 메인 view로 이동(현재 진행중인 스프린트의 tasklist만 조회)
+	@Override
+	public List<TaskHistory> selectTaskList(HashMap<String, Object> map) {
+		return td.selectTaskList(sqlSession, map);
+	}
+
 }
